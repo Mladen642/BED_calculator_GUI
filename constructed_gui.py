f294faad37f4a615d5a3dea80a1bbc0a31ae1164
Mladen <mladenbabic642@protonmail.com>
2023-03-28 20:00:15 +0200

    Fixed window size

--- constructed_gui.py
+++ constructed_gui.py
@@ -14,7 +14,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(618, 455)
+        MainWindow.resize(618, 445)
         MainWindow.setStyleSheet("")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
